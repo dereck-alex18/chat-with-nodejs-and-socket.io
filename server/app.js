bab0acb5a2b2d8fba6fff66ec673412b1c4ce599
dereck-alex18 <dereck_alexander@outlook.com>
2019-02-28 16:37:03 +0100

    add SOCKET.IO to the project

--- server/app.js
+++ server/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
+
 const app = express();
+//setting the port
 let port = process.env.PORT || 3000;
+//creating an http server
+let server = http.createServer(app);
+//adding socket io to the server
+let io = socketIO(server);
+//setting the path to the frontend
 const publicPath = path.join(__dirname, '../public');
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+//Listen for clients
+io.on('connection', (socket) => {
+    console.log('New user connected!');
+
+    //Handles the event when a user disconnect from server
+    socket.on('disconnect', () => {
+        console.log('User disconnected from server!');
+    });
+});
+
+server.listen(port, () => {
     console.log('starting server...');
 });
