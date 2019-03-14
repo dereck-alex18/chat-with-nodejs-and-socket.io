e5073b469d07820d7d66adef447d5040c20c4b92
dereck-alex18 <dereck_alexander@outlook.com>
2019-03-14 17:36:16 +0100

    add notifications sound and autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,7 @@ let socket = io();
 const textForm = document.querySelector('#text');
 const submitBt = document.querySelector('#submitBt');
 const shareLocBtn = document.querySelector('#shareLoc');
+const sidebar = document.querySelector('#sidebar');
 const msgBox = document.querySelector('#messages-box');
 const recordBt = document.querySelector('#recordBt');
 
@@ -12,30 +13,35 @@ const recordBt = document.querySelector('#recordBt');
 const msgTemplate = document.querySelector('#message-template').innerHTML;
 const locTemplate = document.querySelector('#loc-template').innerHTML;
 const audioTemplate = document.querySelector('#audio-template').innerHTML;
+const sideBarTemplate = document.querySelector('#sidebar-template').innerHTML;
 
 //Get the username the the room that the user wants to join
 const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true })
-console.log(username, room);
+
 
 //Listen for a click on submit button
 submitBt.addEventListener('click', (e) => {
     e.preventDefault();
-    //disable submitBt to avoid the message being sent twice
-    submitBt.setAttribute('disabled', 'disabled');
-    //sends the message to the server
-    const message = {from: 'User', text: textForm.value};
-    socket.emit('createMessage', message, (error) => {
-        //This function is called when the data is sent correctly
-        if(error){
-            return console.log(error);
-        }
-        console.log('Delivered!');
-        //enable submit bt again
-        submitBt.removeAttribute('disabled', 'disabled');
-        //To keep the cursor in text form
-        textForm.focus();
-        textForm.value = '';
-    });
+    //Makesure the user do not send empty messages
+    if(textForm.value.trim() !== ''){
+        //disable submitBt to avoid the message being sent twice
+        submitBt.setAttribute('disabled', 'disabled');
+        //sends the message to the server
+        const message = {from: 'User', text: textForm.value};
+        socket.emit('createMessage', message, (error) => {
+            //This function is called when the data is sent correctly
+            //if there's an error the page is refreshed
+            if(error){
+                location.reload();
+            }
+            //enable submit bt again
+            submitBt.removeAttribute('disabled', 'disabled');
+            //To keep the cursor in text form
+            textForm.focus();
+            textForm.value = '';
+        });
+        
+    }
 });
 
 //Listen for a click on share location button
@@ -51,9 +57,13 @@ shareLocBtn.addEventListener('click', () => {
     navigator.geolocation.getCurrentPosition((pos) => {
         let coords = {lat: pos.coords.latitude, lng: pos.coords.longitude};
         
-        socket.emit('shareLoc', coords, () => {
+        socket.emit('shareLoc', coords, (error) => {
+            
             //This function is called when the data is sent correctly
-            console.log('delivered!');
+            //if there's an error, the page is refreshed
+            if(error){
+                location.reload();
+            }
             //enable shareLocBtn
             shareLocBtn.removeAttribute('disabled', 'disabled');
         });   
@@ -62,6 +72,7 @@ shareLocBtn.addEventListener('click', () => {
 
 //Display a msg when the client connects to server
 socket.on('connect', function(){
+    //location.reload();
     console.log('Connected to server!');
     
 });
@@ -75,23 +86,27 @@ socket.on('newMessage', function(message){
     console.log("New message received!");
    //The lines bellow insert the message in the message-box by redering the Mustache template
     const html = Mustache.render(msgTemplate, {
-        user: message.from,
+        user: (message.from !== username) ? message.from : "you",
         message: message.text,
         createdAt: moment(message.createdAt).format('h:mm a')
     });
     msgBox.insertAdjacentHTML('beforeend', html);
+    playSound(message.from);
+    autoscroll();
 });
 
 //Listen for the user's location
 socket.on('shareLocMsg', (message) => {
     //Render the Mustache template for the locarion and insert the url dynamically
     const html = Mustache.render(locTemplate, {
-        username: message.from,
+        username: (message.from !== username) ? message.from : "you",
         url: message.url,
         createdAt: moment(message.createdAt).format('h:mm a')
     })
     //Insert the location in the message box
     msgBox.insertAdjacentHTML('beforeend', html);
+    playSound(message.from);
+    autoscroll();
 });
 
 //Listen for an audio message
@@ -102,14 +117,36 @@ socket.on('audioMsg', (audioMsg) => {
     let url = URL.createObjectURL(audioBlob);
     const html = Mustache.render(audioTemplate, 
         {
-            username: audioMsg.from,
+            username: (audioMsg.from !== username) ? audioMsg.from : "you",
             audioURL: url,
             createdAt: moment(audioMsg.createdAt).format('h:mm a')
         });
     msgBox.insertAdjacentHTML('beforeend', html);
+    playSound(audioMsg.from);
+    autoscroll();
+});
+
+//This event handles updating the sidebar with the usernames and room
+socket.on('roomData', ({room, users}) => {
+   const html = Mustache.render(sideBarTemplate, 
+    {
+        room,
+        users
+    });
+    //update the sidebar when an user join or leave the chat
+    sidebar.innerHTML = html;
+});
+
+socket.emit("join", {username, room}, (error) => {
+    //If there is an error it will be redirected to the initial form
+    if(error){
+        alert(error);
+        location.href = '/';
+    }
     
 });
 
+//Listen for a click on the audio button
 recordBt.addEventListener('click', recordAudioMsg);
 
 let audioChunks = []; //This array will contain the sample of the audio
@@ -123,9 +160,11 @@ function recordAudioMsg(){
         gumStream.getAudioTracks()[0].stop();
         audioChunks = [];
         recordBt.textContent = "Audio";
+        recordBt.style.background = "#7C5CBF"
     } else {
         //Otherwise it starts the recording process
         recordBt.textContent = "Stop";
+        recordBt.style.background = "red";
         navigator.mediaDevices.getUserMedia({
             audio: true
         }).then(function(stream) {
@@ -133,9 +172,12 @@ function recordAudioMsg(){
             recorder = new MediaRecorder(stream);
             recorder.ondataavailable = function(e) {
                 audioChunks.push(e.data); //Save the audio samples in this array
-                socket.emit('audioMsg', audioChunks[0], () => {
-                    //This callback is called when teh server deliver the audio
-                    console.log('Delivered!');
+                socket.emit('audioMsg', audioChunks[0], (error) => {
+                    //This callback is called when the server deliver the audio
+                    //if there is any error the page will be refreshed
+                    if(error){
+                        location.reload();
+                    }
                 });
             };
             recorder.start();
@@ -143,11 +185,44 @@ function recordAudioMsg(){
     }
 }
 
-socket.emit("join", {username, room}, (error) => {
-    //If there is an error it will be redirected to the initial form
-    if(error){
-        alert(error);
-        location.href = '/';
-    }
+//This function handles playing a sound when a new msg is received
+function playSound(currentUser){
+    let bleep = new Audio();
     
-});
+    //if the user who sent the message is different from the current user, the sound will be played
+    //This is to avoid the sound being played when the current user sends a message
+    if(currentUser !== username && currentUser !== 'Admin'){
+        bleep.src = '/sound/msg.mp3';
+        bleep.play();
+    }else if(currentUser === 'Admin'){
+        //If it's an automatic message another sound is played
+        bleep.src = '/sound/join.mp3';
+        bleep.play();
+    }
+}
+
+function autoscroll() {
+    //This function is run everytime a new message is received. It verifies where is the scrollbar
+    //when the message comes. If it is at the very bottom the page scrolls automatically
+
+    //get the last received message
+    const newMessage = msgBox.lastElementChild;
+    //get the height of the message
+    const newMessageStyles = getComputedStyle(newMessage);
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+    const newMessageHeight = newMessage.offsetHeight + newMessageMargin;
+    
+    //visible height
+    const visibleHeight = msgBox.offsetHeight;
+    //container height
+    const containerHeight = msgBox.scrollHeight;
+    //How far down have I scrolled
+    const scrollOffSet = msgBox.scrollTop + visibleHeight;
+
+    //This if statement verifies if the scroll bar is at the very bottom
+    //if so, the page is scrolled down automatically, otherwise it'll not be
+    if(containerHeight - newMessageHeight <= scrollOffSet){
+        msgBox.scrollTop = msgBox.scrollHeight;
+    }
+}
+
--- server/app.js
+++ server/app.js
@@ -14,8 +14,6 @@ let server = http.createServer(app);
 let io = socketIO(server);
 //setting the path to the frontend
 const publicPath = path.join(__dirname, '../public');
-
-
 app.use(express.static(publicPath));
 
 //Listen for clients
@@ -27,28 +25,38 @@ io.on('connection', (socket) => {
         //Makesure the user exist in order to not crash the server side code
         if(user){
             socket.broadcast.to(user.room).emit('newMessage', formatMessages('Admin', `${user.username} has left!`));
+            //This event will update the users' list on the client side
+            io.to(user.room).emit("roomData", {room: user.room, users: getUsersInRoom(user.room)});
         }
        
     });
 
    //Listen for an event, when a message is created by the client
     socket.on('createMessage', (message, callback) => {
-        //get the username and from from getUser function
-        const {username, room} = getUser(socket.id);
-        
-        socket.join(room);
+        //get the user from getUser function
+        const user = getUser(socket.id);
+        //If the user does not exist, the callback will be returned, preventing the server from crashing
+        if(!user){
+           return callback(true); 
+        }
+        socket.join(user.room);
         //create an message object with who sent it, the message itself and when it was sent
-        message = formatMessages(username, message.text);
+        message = formatMessages(user.username, message.text);
         //When a new message is sent by an user, it will be sent to all other users
-        io.to(room).emit('newMessage', message);
+        io.to(user.room).emit('newMessage', message);
         callback(); //It tells the client that everything went well
     });
 
     //Listen for a shareLoc (share location) event. 
     socket.on('shareLoc', (pos, callback) => {
+        const user = getUser(socket.id);
+        //If the user does not exist, the callback will be returned, preventing the server from crashing
+        if(!user){
+            return callback(true);
+        }
         //get the username and from from getUser function
-        const {username, room} = getUser(socket.id);
-
+        const {username, room} = user;
+        
         socket.join(room);
         // When an user share its location it will be sent to the other users through an url
         io.to(room).emit('shareLocMsg', formatShareLoc(username, `https://www.google.com/maps?q=${pos.lat},${pos.lng}`));
@@ -58,8 +66,14 @@ io.on('connection', (socket) => {
     
     //Listen for an audioMsg event
     socket.on('audioMsg', (audioMsg, callback) => {
+        const user = getUser(socket.id);
+        //If the user does not exist, the callback will be returned, preventing the server from crashing
+        if(!user){
+            return callback(true);
+        }
+
         //get the username and from from getUser function
-        const {username, room} = getUser(socket.id);
+        const {username, room} = user;
         
         //make sure the message is sent only to the current room
         socket.join(room);
@@ -76,12 +90,13 @@ io.on('connection', (socket) => {
         }
 
         socket.join(user.room);
-        currentUser = username
         //When a new user joins the chat, a welcome message is sent to him
         socket.emit('newMessage', formatMessages('Admin', `Welcome ${user.username} :)`));
         //The following message will be shown to all users but the current one
         socket.broadcast.to(user.room).emit('newMessage', formatMessages('Admin', `${user.username} joined the chat!`));
-        
+        //This event will update the users' list on the client side
+        io.to(room).emit("roomData", {room: user.room, users: getUsersInRoom(user.room)});
+
         callback();
     });
 
